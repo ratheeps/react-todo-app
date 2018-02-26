1bb8e17342284ac0e004e2420c950212cea22c49
Ratheep <ratheepan93@hotmail.com>
2018-02-26 20:12:02 +0530

    task update

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -5,17 +5,17 @@ export const TaskService = {
     index,
 };
 
-function index() {
-    let accessToken = localStorage.getItem('uid');
-    const requestOptions = {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-    };
+let accessToken = localStorage.getItem('uid');
+const requestOptions = {
+    method: 'GET',
+    headers: {
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+};
 
+function index() {
     let AppUrl = API_URL + 'api/tasks';
     return fetch(AppUrl, requestOptions).then((response) => response.json())
         .then((responseJson) => {
